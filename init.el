53e1bcfa1d96549b9c0717b61f6bc0ff61bdecb9
erfurth <erfurth@pop-os.localdomain>
2023-05-10 09:38:49 +0800

    dirvish and matlab

--- init.el
+++ init.el
@@ -44,6 +44,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   '(((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "PDF Tools")
+     (output-html "xdg-open")))
  '(ansi-color-names-vector
    ["#282c34" "#ff6c6b" "#98be65" "#da8548" "#61afef" "#c678dd" "#1f5582" "#abb2bf"])
  '(avy-keys '(113 119 101 97 115 100 122 120 99))
@@ -55,8 +63,13 @@
  '(eyebrowse-mode t)
  '(git-gutter:hide-gutter t)
  '(git-gutter:update-interval 2)
+ '(helm-completion-style 'helm)
+ '(helm-ff-lynx-style-map t)
+ '(helm-imenu-lynx-style-map t t)
+ '(helm-occur-use-ioccur-style-keys t)
  '(hippie-expand-try-functions-list
    '(yas-hippie-try-expand try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol))
+ '(ispell-dictionary nil)
  '(lsp-ltex-version nil)
  '(org-babel-load-languages '((emacs-lisp . t) (python . t) (java . t) (R . t)))
  '(org-file-apps
@@ -96,7 +109,7 @@
      ("v" . "verse")
      ("b" . "src emacs-lisp")))
  '(package-selected-packages
-   '(doom-themes doom-modeline nano-theme blamer hc-zenburn-theme zenburn-theme mu4e-alert org-mu4e mu4e helm-sage sage-shell-mode dirvish org-attach-screenshot diff-hl lsp-pyright diredfl js2-refactor emacs-everywhere everywhere helm-navi lsp-ltex lsp-ui company-lsp lsp-mode js2-mode json-mode org-tree-slide embark marginalia elpy org pdf-tools org-pdftools memory-usage i3wm-config-mode bufler goto-chg emojify lorem-ipsum helm-config use-package-chords volatile-highlights ag hydra xkcd company-math git git-gutter diminish org-noter ox-reveal ox-twbs org-bullets htmlize sudo-edit magithub magit-todos magit company-bibtex auctex-latexmk cdlatex company-auctex auctex company-shell company-lua slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer company flycheck yasnippet-snippets yasnippet auto-dictionary zzz-to-char hungry-delete rainbow-delimiters rainbow-mode beacon multiple-cursors undo-tree expand-region avy swiper eyebrowse switch-window which-key ivy-historian historian spaceline winum dashboard projectile all-the-icons-dired restart-emacs async pretty-mode zerodark-theme use-package))
+   '(matlab-mode latex-table-wizard workgroups2 doom-themes doom-modeline nano-theme blamer hc-zenburn-theme zenburn-theme mu4e-alert org-mu4e mu4e helm-sage sage-shell-mode dirvish org-attach-screenshot diff-hl lsp-pyright diredfl js2-refactor emacs-everywhere everywhere helm-navi lsp-ltex lsp-ui company-lsp lsp-mode js2-mode json-mode org-tree-slide embark marginalia elpy org pdf-tools org-pdftools memory-usage i3wm-config-mode bufler goto-chg emojify lorem-ipsum helm-config use-package-chords volatile-highlights ag hydra xkcd company-math git git-gutter diminish org-noter ox-reveal ox-twbs org-bullets htmlize sudo-edit magithub magit-todos magit company-bibtex auctex-latexmk cdlatex company-auctex auctex company-shell company-lua slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer company flycheck yasnippet-snippets yasnippet auto-dictionary zzz-to-char hungry-delete rainbow-delimiters rainbow-mode beacon multiple-cursors undo-tree expand-region avy swiper eyebrowse switch-window which-key ivy-historian historian spaceline winum dashboard projectile all-the-icons-dired restart-emacs async pretty-mode zerodark-theme use-package))
  '(sage-shell:use-prompt-toolkit nil)
  '(sage-shell:use-simple-prompt t)
  '(use-file-dialog nil)
